bd44e0d4c6bfb44e20ea65f71df3bc6bc5d1fe7c
Jibao Mansaray <jibaomansaray@gmail.com>
2023-03-08 14:35:45 +0000

    Add a `users` route and handler
    This route is use for quickly testing the return json
    data.

--- bin/dirtybase/src/main.rs
+++ bin/dirtybase/src/main.rs
@@ -18,13 +18,31 @@ async fn main() -> std::io::Result<()> {
 
     let x = web::Data::new(app);
 
-    HttpServer::new(move || App::new().app_data(x.clone()).service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(x.clone())
+            .service(hello)
+            .service(serve_users)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/users")]
+async fn serve_users(app: web::Data<Dirtybase>) -> impl Responder {
+    let mut manager = app.schema_manger();
+    let result = manager
+        .table("_core_users", |query| {
+            query.is_in("internal_id", vec![2, 1, 40]);
+        })
+        .fetch_all_as_json()
+        .await;
+
+    HttpResponse::Ok().json(result)
 }
