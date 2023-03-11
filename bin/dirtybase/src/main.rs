437848398ab9a5b4f1082a5c5e5dee88b17c3813
Jibao Mansaray <jibaomansaray@gmail.com>
2023-03-11 08:40:47 +0000

    Move `env` setup to main
    Also sourcing more configuration from the env variables.
    Having the env setup here makes them easily visible.

--- bin/dirtybase/src/main.rs
+++ bin/dirtybase/src/main.rs
@@ -1,30 +1,57 @@
 pub mod app;
 pub mod http;
 
+use std::env;
+
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use app::app_setup::Dirtybase;
+use dotenv::dotenv;
+use log::{error, info};
+use pretty_env_logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app = Dirtybase::new().await.unwrap();
+    pretty_env_logger::init();
 
-    app.db_setup().await;
-
-    if app.schema_manger().has_table("students").await {
-        println!("table exist");
-    } else {
-        println!("table does not exist");
+    if let Err(e) = dotenv() {
+        error!("could not load .env file: {:#}", e);
     }
 
-    let x = web::Data::new(app);
+    let db_connection = if let Ok(conn) = env::var("DTY_DATABASE") {
+        conn
+    } else {
+        error!("error");
+        "".to_owned()
+    };
+
+    let max_connection: u32 = if let Ok(max) = env::var("DTY_DATABASE_MAX_POOL_CONNECTION") {
+        max.parse().unwrap_or(5)
+    } else {
+        5
+    };
+
+    let app = Dirtybase::new(&db_connection, max_connection)
+        .await
+        .unwrap();
+    app.db_setup().await;
+
+    let data = web::Data::new(app);
+    let port: u16 = if let Ok(p) = env::var("DTY_WEB_PORT") {
+        p.parse().unwrap_or(8080)
+    } else {
+        8080
+    };
+
+    info!("Server running on port: {}", port);
+    info!("DB max pool: {}", db_connection);
 
     HttpServer::new(move || {
         App::new()
-            .app_data(x.clone())
+            .app_data(data.clone())
             .service(hello)
             .service(serve_users)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
